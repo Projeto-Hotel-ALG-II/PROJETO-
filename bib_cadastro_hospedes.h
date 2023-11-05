d3cc2618c776c09020339161182b2561388d413c
Matheus Garcia Batista <bmatheusgarcia@gmail.com>
2023-11-04 23:53:23 -0300

    Adicionando seção de cadastro deo hóspedes

--- bib_cadastro_hospedes.h
+++ bib_cadastro_hospedes.h
@@ -19,5 +19,24 @@ typedef struct
     char data_nasc[11]; // DD/MM/AAAA
 } str_hospedes;
 
+/*
+ * Cadastra o H¢spede (Parƒmetro: struct com todos os dados)
+ */
+int cadastrarHospede(int mode, str_hospedes hospedes);
+
+/*
+ * Pesquisa o H¢spede (precisa ser chamado com os endere‡os de mem¢ria de todos as vari veis)
+ */
+int pesquisarHospede(int mode, char pesQ_cpf[15], str_hospedes *pHosped);
+
+/*
+ * Faz a altera‡Æo do h¢spede (selecionado pelo CPF)
+ */
+int alterarHospede(int mode, char cpf[15], str_hospedes att_hosped);
+
+/*
+ * Exclui o h¢spede selecionado
+ */
+int excluirHospede(int mode, char cpf[15]);
 
 #endif
